a19f1d322a4c1924b995153173aa85ecfbc2c1d6
andy <andy.william.mcdonald@gmail.com>
2020-01-23 12:24:32 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import JamJar from "./Components/JamJar";
+import { Container, Button } from "@material-ui/core";
+import styled, { keyframes } from "styled-components";
+import "./App.css";
+
+const wiggle = keyframes`
+// from {
+//   transform: rotate(-10deg);
+// }
+// to {
+//   transform: rotate(10deg);
+// }
+0% { transform: translate(1px, 1px) rotate(0deg); }
+10% { transform: translate(-1px, -2px) rotate(-1deg); }
+20% { transform: translate(-3px, 0px) rotate(1deg); }
+30% { transform: translate(3px, 2px) rotate(0deg); }
+40% { transform: translate(1px, -1px) rotate(1deg); }
+50% { transform: translate(-1px, 2px) rotate(-1deg); }
+60% { transform: translate(-3px, 1px) rotate(0deg); }
+70% { transform: translate(3px, 1px) rotate(-1deg); }
+80% { transform: translate(-1px, -1px) rotate(1deg); }
+90% { transform: translate(1px, 2px) rotate(0deg); }
+100% { transform: translate(1px, -2px) rotate(-1deg); }
+`;
+
+const JamContainer = styled(Container)`
+  // border: 1px solid black;
+  height: 80vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  // object-fit: cover;
+  animation: ${wiggle} infinite 1s linear;
+`;
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Pump Up The Jam</h1>
+      <h3>Score: {count}</h3>
+      <JamContainer maxWidth="md">
+        <JamJar count={count} />
+      </JamContainer>
+      <Button onClick={() => setCount(count + 1)}>Pump</Button>
     </div>
   );
 }
