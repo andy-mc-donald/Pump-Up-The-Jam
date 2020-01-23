308676b9ae94ffdbe833c9a60b40a9adc5d33927
Jack Bridger <jacksbridger@gmail.com>
2020-01-23 16:53:18 +0000

    add splat

--- src/Pages/NotInPlay/NotInPlay.js
+++ src/Pages/NotInPlay/NotInPlay.js
@@ -1,21 +1,23 @@
 import React from "react";
 // import { Box } from "@material-ui/core";
-import {BoxContainer, EndTitle, ButtonRestart } from "./NotInPlay.style";
+import { BoxContainer, EndTitle, ButtonRestart } from "./NotInPlay.style";
+import Splat from "./Splat"
 function NotInPlay({ count, setCount, setInPlay }) {
-  const restartGame = () => {
-    setCount(0);
-    setInPlay(true);
-  };
-  return (
-    <BoxContainer>
-      <EndTitle variant="h3" className="main-title">
-        <span>Y</span>
-        <span>o</span><span>u</span><span>r</span><span> S</span><span>c</span><span>o</span><span>r</span><span>e</span>
-        <span> W</span><span>a</span><span>s</span> {count}
-      </EndTitle>
-      <ButtonRestart size="large" textsize="large" onClick={restartGame}>Restart</ButtonRestart>
-    </BoxContainer>
-  );
+    const restartGame = () => {
+        setCount(0);
+        setInPlay(true);
+    };
+    return (
+        <BoxContainer>
+            <EndTitle variant="h3" className="main-title">
+                <span>Y</span>
+                <span>o</span><span>u</span><span>r</span><span> S</span><span>c</span><span>o</span><span>r</span><span>e</span>
+                <span> W</span><span>a</span><span>s</span> {count}
+            </EndTitle>
+            <Splat />
+            <ButtonRestart size="large" textsize="large" onClick={restartGame}>Restart</ButtonRestart>
+        </BoxContainer>
+    );
 }
 
 export default NotInPlay;
--- src/Pages/NotInPlay/NotInPlay.style.js
+++ src/Pages/NotInPlay/NotInPlay.style.js
@@ -1,5 +1,26 @@
-import styled from 'styled-components';
-import {Box, Typography, Button} from '@material-ui/core'
+import styled, { keyframes } from 'styled-components';
+import { Box, Typography, Button } from '@material-ui/core'
+
+const wiggle = keyframes`
+0% { transform: translate(1px, 1px) rotate(0deg); }
+10% { transform: translate(-1px, -2px) rotate(-1deg); }
+20% { transform: translate(-3px, 0px) rotate(1deg); }
+30% { transform: translate(3px, 2px) rotate(0deg); }
+40% { transform: translate(1px, -1px) rotate(1deg); }
+50% { transform: translate(-1px, 2px) rotate(-1deg); }
+60% { transform: translate(-3px, 1px) rotate(0deg); }
+70% { transform: translate(3px, 1px) rotate(-1deg); }
+80% { transform: translate(-1px, -1px) rotate(1deg); }
+90% { transform: translate(1px, 2px) rotate(0deg); }
+100% { transform: translate(1px, -2px) rotate(-1deg); }
+`;
+
+const SplatContainer = styled(Box)`
+  animation: ${wiggle} infinite 2s linear;
+  height: 20vh;
+
+`
+
 
 const BoxContainer = styled(Box)`
     // background-color: black;
@@ -61,4 +82,4 @@ const ButtonRestart = styled(Button)`
 
 
 
-export {BoxContainer, EndTitle, ButtonRestart };
+export { BoxContainer, EndTitle, ButtonRestart, SplatContainer };
